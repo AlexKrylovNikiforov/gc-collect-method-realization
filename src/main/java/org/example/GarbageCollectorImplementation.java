451836bdac25341dfe0c697ae8b120b8ac413620
AlexKrylovNikiforov <gomezkorta@gmail.com>
2023-09-20 15:09:16 +0100

    comments added

--- src/main/java/org/example/GarbageCollectorImplementation.java
+++ src/main/java/org/example/GarbageCollectorImplementation.java
@@ -6,6 +6,11 @@ import java.util.*;
 
 public class GarbageCollectorImplementation implements GarbageCollector {
 
+  /*
+  идея в том, что объекты, которые содержатся в куче, могут считаться живыми только только если на них есть ссылка из стека, живыми являются также все объекты,
+  которые непосредственно содердатся в стеке. Поэтому мы проверяем каждый объект из стека, и рекурсивно - объекты, составляющие его поля, на предмет наличия ссылок на какие-либо
+  объекты из кучи, и если такие есть - объект из кучи помещается в reachableBeans (маркируется как живой), остальные объекты являются неживыми и могут быть удалены
+   */
   @Override
   public List<ApplicationBean> collect(HeapInfo heap, StackInfo stack) {
     Map<String, ApplicationBean> beans = heap.getBeans();
@@ -26,7 +31,9 @@ public class GarbageCollectorImplementation implements GarbageCollector {
     }
     return actualGarbage;
   }
-
+  /*
+  в данном методе мы проходим в итерации по сету из объектов, которые мы считаем "живыми" и если объект отсутствует среди живых, проверяем также рекурсивно все его поля
+   */
   private void addReachableBeans(Set<ApplicationBean> reachableObjects, ApplicationBean bean) {
     if(!reachableObjects.contains(bean)) {
       reachableObjects.add(bean);
